f20976c7d6f5a08cdfa268084bd14e1e986f9af0
we.sy0x26ler <salcides741@gmail.com>
2025-02-21 22:25:45 -0300

    atu

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function clickMenu(){
     } else{
         itens.style.display = 'block'
     }
-}/*fim do menu*/
+}
+/*fim do menu*/
 let trilho = document.getElementById('trilho')
 let section = document.querySelector('section')
 trilho.addEventListener('click', ()=>{
